bc5ae7903ef1fb620d22c400efd0d5a459ed08f2
lafree317 <lafree317@163.com>
2016-05-21 11:39:00 +0800

    更新了需要再次更改cell高度时候的方法

--- ZEMVVM/ZEMVVM/View/ZECell.swift
+++ ZEMVVM/ZEMVVM/View/ZECell.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ZECell: UITableViewCell {
 
+    @IBOutlet weak var labelButtomConstaranits: NSLayoutConstraint!
     @IBOutlet weak var bigTitleLabel: UILabel!
     @IBOutlet weak var contextLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        let randomNumber = Int.random(0, 1)
+        if randomNumber == 0 {
+            labelButtomConstaranits.constant = 100
+            self.layoutIfNeeded()
+        }
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
@@ -23,4 +29,17 @@ class ZECell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+}
+
+// 随机数...
+public extension Int {
+    /// SwiftRandom extension
+    public static func random(range: Range<Int>) -> Int {
+        return random(range.endIndex, range.startIndex)
+    }
+    
+    /// SwiftRandom extension
+    public static func random(lower: Int = 0, _ upper: Int = 100) -> Int {
+        return lower + Int(arc4random_uniform(UInt32(upper - lower + 1)))
+    }
 }
